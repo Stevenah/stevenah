38f090654649cc111c1e5d0b241b8a781a18b151
Stevenah <shicks1000@gmail.com>
2017-08-12 19:52:58 +0200

    update

--- build.production.js
+++ build.production.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 var ENVIRONMENT = {
-    'process.env.NODE_ENV': JSON.stringify('DEVELOPMENT'),
+    'process.env.NODE_ENV': JSON.stringify('PRODCUTION'),
 }
 
 var BUILD_PATH = path.resolve(__dirname, 'build/');
@@ -11,34 +11,31 @@ var APP_PATH = path.resolve(__dirname, 'src/');
 var PORT = 8080;
 
 var config = {
-    entry: {
+    entry: [
         'webpack-dev-server/client?http://0.0.0.0:8080',
         'webpack/hot/only-dev-server',
         'whatwg-fetch',
-        path.resolve(APP_PATH, 'app.js')
-    },
+        path.resolve(APP_PATH, 'js/index.js')
+    ],
     output: {
         path: BUILD_PATH,
         filename: 'app.bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.(js|jsx)$/, loaders: ['babel'], exclude: /node_modules/ },
-            { test: /\.(scss|css)$/, loaders: ['style', 'css', 'sass'] },
-            { test: /\.(eot|woff2|woff|ttf|svg)$/, loaders: ['url'] },
-            { test: /\.json$/, loader: 'json' },
+            { test: /\.(js|jsx)$/, loaders: ['babel-loader'], exclude: /node_modules/ },
+            { test: /\.(scss|css)$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] },
+            { test: /\.(eot|woff2|woff|ttf|svg)$/, loaders: ['url-loader'] },
+            { test: /\.json$/, loader: 'json-loader' },
         ],
     },
     resolve: {
-        root: [
-            path.resolve('./webapp/js'),
-        ],
-    },
-    devserver: {
-        contentBase: APP_PATH,
-        port: PORT,
-        inline: true,
-        hot: true,
+        alias: {
+            app: path.resolve(__dirname, 'src/js/components/app'),
+            ui: path.resolve(__dirname, 'src/js/components/ui'),
+            pages: path.resolve(__dirname, 'src/js/components/pages'),
+            styles: path.resolve(__dirname, 'src/scss'),
+        },
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
