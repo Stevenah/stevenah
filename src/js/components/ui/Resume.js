2304ebdade180b79dd4b905b75f55bc4f54a5d0a
Stevenah <shicks1000@gmail.com>
2017-08-12 19:46:07 +0200

    update

--- src/js/components/ui/Resume.js
+++ src/js/components/ui/Resume.js
@@ -12,27 +12,6 @@ class Resume extends Component {
                         <a id="link-3" className="twitter" href="https://twitter.com/DeFactoSteven">twitter</a>
                     </div>
                 </div>
-                <div className="content-group">
-                    <h2>Work</h2>
-                    <div>
-                        <a id="link-4" href="#">Axiom Norway AS, 2014 - 2016</a>
-                        <a id="link-5" href="#">University of Oslo, 2016 - present</a>
-                    </div>
-                </div>
-                <div className="content-group">
-                    <h2>Technical</h2>
-                    <div>
-                        <a id="link-6" href="#"></a>
-                        <a id="link-7" href="#"></a>
-                    </div>
-                </div>
-                <div className="content-group">
-                    <h2>Education</h2>
-                    <div>
-                        <a id="link-6" href="#">University of Agder (BSc. Information Systems)</a>
-                        <a id="link-7" href="#">University of Oslo (MSc. Computer Science)</a>
-                    </div>
-                </div>
             </div>
         );
     }
